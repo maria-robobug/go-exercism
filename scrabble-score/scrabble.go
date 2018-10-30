24c964f5302b4fd75e10a265d97f4ead59386d42
maria-robobug <mariashow2001@gmail.com>
2018-10-30 20:30:27 +0000

    Scrabble

--- scrabble-score/scrabble.go
+++ scrabble-score/scrabble.go
@@ -1,29 +1,44 @@
 package scrabble
 
-import (
-	"strings"
-)
+import "strings"
 
-// Score - Given a word, compute scrabble scrore
-func Score(word string) (score int) {
-	table := map[int][]string{
-		1:  []string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"},
-		2:  []string{"D", "G"},
-		3:  []string{"B", "C", "M", "P"},
-		4:  []string{"F", "H", "V", "W", "Y"},
-		5:  []string{"K"},
-		8:  []string{"J", "X"},
-		10: []string{"Q", "Z"},
-	}
+var values = map[string]int{
+	"a": 1,
+	"e": 1,
+	"i": 1,
+	"o": 1,
+	"u": 1,
+	"l": 1,
+	"n": 1,
+	"r": 1,
+	"s": 1,
+	"t": 1,
+	"d": 2,
+	"g": 2,
+	"b": 3,
+	"c": 3,
+	"m": 3,
+	"p": 3,
+	"f": 4,
+	"h": 4,
+	"v": 4,
+	"w": 4,
+	"y": 4,
+	"k": 5,
+	"j": 8,
+	"x": 8,
+	"q": 10,
+	"z": 10,
+}
 
-	for _, c := range word {
-		for k, v := range table {
-			for _, l := range v {
-				if strings.ToLower(string(c)) == strings.ToLower(l) {
-					score += k
-				}
-			}
-		}
+// Score - Given a word, compute scrabble score
+func Score(input string) (score int) {
+	input = strings.ToLower(input)
+
+	var w string
+	for _, c := range input {
+		w = string(c)
+		score += values[w]
 	}
 
 	return score
