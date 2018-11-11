731b2182afefe23845c569317d9ddf67a0a265ea
maria-robobug <mariashow2001@gmail.com>
2018-11-11 13:33:48 +0000

    Use runes to store input instead

--- scrabble-score/scrabble.go
+++ scrabble-score/scrabble.go
@@ -1,44 +1,42 @@
 package scrabble
 
-import "strings"
+import "unicode" 
 
-var values = map[string]int{
-	"a": 1,
-	"e": 1,
-	"i": 1,
-	"o": 1,
-	"u": 1,
-	"l": 1,
-	"n": 1,
-	"r": 1,
-	"s": 1,
-	"t": 1,
-	"d": 2,
-	"g": 2,
-	"b": 3,
-	"c": 3,
-	"m": 3,
-	"p": 3,
-	"f": 4,
-	"h": 4,
-	"v": 4,
-	"w": 4,
-	"y": 4,
-	"k": 5,
-	"j": 8,
-	"x": 8,
-	"q": 10,
-	"z": 10,
+var values = map[rune]int{
+	'a': 1,
+	'e': 1,
+	'i': 1,
+	'o': 1,
+	'u': 1,
+	'l': 1,
+	'n': 1,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'd': 2,
+	'g': 2,
+	'b': 3,
+	'c': 3,
+	'm': 3,
+	'p': 3,
+	'f': 4,
+	'h': 4,
+	'v': 4,
+	'w': 4,
+	'y': 4,
+	'k': 5,
+	'j': 8,
+	'x': 8,
+	'q': 10,
+	'z': 10,
 }
 
 // Score - Given a word, compute scrabble score
 func Score(input string) (score int) {
-	input = strings.ToLower(input)
-
-	var w string
+  var w rune
 	for _, c := range input {
-		w = string(c)
-		score += values[w]
+    w = unicode.ToLower(c)
+    score += values[w]
 	}
 
 	return score
